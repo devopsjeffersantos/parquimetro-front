ad516812a8c5f13813a15479600c025e49e7ad08
jefferson <devops.jeffersantos@gmail.com>
2024-01-30 02:12:52 -0300

    ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -16,7 +16,7 @@ export const routes: Routes = [
     { path: 'tempos', component: CadastroTempoComponent },
     { path: 'pagamentos', component: PagamentoComponent },
     { path: 'pagamentos/:id', component: PagamentoComponent },
-    { path: 'recibos/:id', component:  ReciboComponent}
+    { path: 'recibos/:id,:id', component:  ReciboComponent}
 ];
 
 @NgModule({
