425097872c015ff63c7511eee920abf249ac81b4
jefferson <devops.jeffersantos@gmail.com>
2024-01-30 23:41:02 -0300

    ok

--- src/app/view/recibo/recibo.component.ts
+++ src/app/view/recibo/recibo.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import {NgForOf} from "@angular/common";
+import {HttpClient} from "@angular/common/http";
 import {PagamentoService} from "../../service/pagamento/pagamento.service";
 import {TempoService} from "../../service/tempo/tempo.service";
 import {CondutorService} from "../../service/condutor/condutor.service";
@@ -22,29 +23,29 @@ import {Veiculo} from "../../model/veiculo/veiculo";
 })
 export class ReciboComponent implements OnInit {
 
-    pagamentoId!: number;
-    pagamento: Pagamento = new Pagamento();
-    tempo: Tempo = new Tempo();
-    condutor: Condutor = new Condutor();
-    veiculo: Veiculo = new Veiculo();
+  pagamentoId!: number;
+  pagamento: Pagamento = new Pagamento();
+  tempo: Tempo = new Tempo();
+  condutor: Condutor = new Condutor();
+  veiculo: Veiculo = new Veiculo();
 
-    constructor(
-        private route: ActivatedRoute,
-        private service: PagamentoService,
-        private tempoService: TempoService,
-        private condutorService: CondutorService,
-        private pagamentoService: PagamentoService,
-        private veiculoService: VeiculoService,
-    ) {
+  constructor(
+      private route: ActivatedRoute,
+      private service: PagamentoService,
+      private tempoService: TempoService,
+      private condutorService: CondutorService,
+      private pagamentoService: PagamentoService,
+      private veiculoService: VeiculoService,
 
-    }
+  ) {
 
-    ngOnInit(): void {
-        this.pagamentoId = +(this.route.snapshot.paramMap.get("id") || 0);
-        this.getPagamento();
-        this.getTempo();
-        this.getVeiculo();
-        this.getCondutor();
+  }
+   ngOnInit(): void {
+    this.pagamentoId= +(this.route.snapshot.paramMap.get("id") || 0);
+       this.getPagamento();
+       this.getTempo();
+       this.getVeiculo();
+       this.getCondutor();
     }
 
     getPagamento() {
@@ -58,7 +59,6 @@ export class ReciboComponent implements OnInit {
         });
     }
 
-
     getTempo() {
         this.tempoService.getAll().subscribe({
             next: (response) => {
@@ -70,6 +70,16 @@ export class ReciboComponent implements OnInit {
         });
     }
 
+    getCondutor() {
+        this.condutorService.getAll().subscribe({
+            next: (response) => {
+                this.condutor = response.find((c) => c.id == this.tempo.condutor)!;
+            },
+            error: (responseError) => {
+                console.log("error: " + JSON.stringify(responseError));
+            },
+        });
+    }
 
     getVeiculo() {
         this.veiculoService.getAll().subscribe({
@@ -82,15 +92,4 @@ export class ReciboComponent implements OnInit {
         });
     }
 
-
-    getCondutor() {
-        this.condutorService.getAll().subscribe({
-            next: (response) => {
-                this.condutor = response.find((c) => c.id == this.tempo.condutor)!;
-            },
-            error: (responseError) => {
-                console.log("error: " + JSON.stringify(responseError));
-            },
-        });
-    }
 }
--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,5 @@
 
 export const environment = {
     production: false,
-    apiUrlBase: 'https://desolate-woodland-89756-5f475fea28a4.herokuapp.com'
+    apiUrlBase: 'http://localhost:8080'
   };
